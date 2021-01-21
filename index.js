7778e3c9d0a398cfe819242efed53cddb36a6974
joelruiz2 <joelruiz2@gmail.com>
2021-01-20 21:30:50 -0600

    adjusted testing

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function updatePlayerCards(id, name, cards) {
 async function testGame1() {
     let id = await holdem.newgame();
     //console.log(id);
-    await holdem.playerjoin(id, 'Joel', 100)
+    await holdem.playerjoin(id, 'Joel', 90)
     await holdem.playerjoin(id, 'Tim', 100)
     await holdem.playerjoin(id, 'Bob', 100)
     await holdem.playerjoin(id, 'Bob2', 100)
@@ -59,46 +59,51 @@ async function testGame1() {
     await holdem.newround(id);
 
 
-    await updatePlayerCards(id, 'Joel', ['7S', 'QD']);
+    await updatePlayerCards(id, 'Joel', ['4D', 'WW']);
     await updatePlayerCards(id, 'Tim', ['4C', '9H']);
     await updatePlayerCards(id, 'Bob', ['2S', '2C']);
     await updatePlayerCards(id, 'Bob2', ['2H', '6S']);
     await updatePlayerCards(id, 'Bob3', ['TD', 'KS']);
     await updatePlayerCards(id, 'Bob4', ['4S', 'WW']);
-    await updatePlayerCards(id, 'Bob5', ['6D', '8D']);
+    await updatePlayerCards(id, 'Bob5', ['7S', 'QD']);
 
-    await holdem.action(id, 'raise/2'); //p1
-    await holdem.action(id, 'raise/4'); //p2
+
+
+    await holdem.action(id, 'fold'); //p1
+    await holdem.action(id, 'fold'); //p2
     await holdem.action(id, 'fold'); //p3
-    await holdem.action(id, 'call'); //p4
-    await holdem.action(id, 'call'); //p5
-    await holdem.action(id, 'call'); //p6
-    await holdem.action(id, 'call'); //p7
-    await holdem.action(id, 'call'); //p8
-    //await holdem.action(id, 'call');
+    await holdem.action(id, 'raise/90'); //p4
+    await holdem.action(id, 'raise/6'); //p5
+    await holdem.action(id, 'fold'); //p6
 
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
+    await holdem.action(id, 'fold'); //p7
 
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
+    // await holdem.action(id, 'call'); //p8
 
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    await holdem.action(id, 'check');
-    updateTableCards(id, ['9S', 'JC', 'KD', '3S', '9D']);
+    // await holdem.action(id, 'call');
 
-    await holdem.action(id, 'check');
+    //await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+
+    // //await holdem.action(id, 'check');
+    // // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+
+    // //await holdem.action(id, 'check');
+    // // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // await holdem.action(id, 'check');
+    // updateTableCards(id, ['9S', 'JC', 'KD', '3S', '9D']);
+
+    // await holdem.action(id, 'check');
 
     printParams(id);
 }
